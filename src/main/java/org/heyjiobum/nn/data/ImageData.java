e4ae4f9f384fa4fb1b1a607cd9b085d30c3890ec
HeyJIOBUM <kirill.nichinger@gmail.com>
2024-12-12 00:13:33 +0300

    add image data

---
+++ src/main/java/org/heyjiobum/nn/data/ImageData.java
@@ -0,0 +1,17 @@
+package org.heyjiobum.nn.data;
+
+public class ImageData {
+    public int imageClass;
+    public double[] image;
+
+    public ImageData(double[] image, int imageClass){
+        this.image = image;
+        this.imageClass = imageClass;
+    }
+
+    public double[] getOneHotClass() {
+        double[] result = new double[10];
+        result[imageClass - 1] = 1;
+        return result;
+    }
+}
